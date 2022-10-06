d16c8995d4d4d3a098653b0ae908c525c991da6e
Laure-C <55760660+Laure-C@users.noreply.github.com>
2022-10-06 23:03:13 +0200

    1 file modified

--- src/weatherByCity.ts
+++ src/weatherByCity.ts
@@ -3,7 +3,43 @@ import { CityWeatherModel } from './CityWeatherModel.model';
 class _WeatherByCityService {
   private allWeather: CityWeatherModel[] = [];
 
-  addACityWeather() {}
+  addACityWeather() {
+    this.allWeather.push(
+      {
+        cityName: 'Grenoble',
+        country: 'France',
+        temperature: 22,
+        summary: 'Ensoleillé, très peu de vent',
+        icon: 'clear-day',
+        date: new Date(),
+        precipitation: 0,
+        vent: 6,
+        humidité: 96,
+      },
+      {
+        cityName: 'Lyon',
+        country: 'France',
+        temperature: 22,
+        summary: 'Partiellement nuageux , très peu de vent',
+        icon: 'partly-cloudy-day',
+        date: new Date(),
+        precipitation: 0,
+        vent: 7,
+        humidité: 92,
+      },
+      {
+        cityName: 'Oulles',
+        country: 'France',
+        temperature: 15,
+        summary: 'Ensoleillé , très peu de vent',
+        icon: 'clear-day',
+        date: new Date(),
+        precipitation: 0,
+        vent: 5,
+        humidité: 87,
+      }
+    );
+  }
 
   getAllWeather(): CityWeatherModel[] {
     return this.allWeather;
