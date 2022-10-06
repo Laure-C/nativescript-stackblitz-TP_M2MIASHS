30025af5f63936384d6560bfd7e7f6ee6630c20f
Laure-C <55760660+Laure-C@users.noreply.github.com>
2022-10-06 23:11:22 +0200

    2 files modified

--- src/components/ScreenOne.tsx
+++ src/components/ScreenOne.tsx
@@ -1,46 +1,43 @@
 import { Dialogs } from '@nativescript/core';
 import { RouteProp } from '@react-navigation/core';
-import * as React from "react";
-import { StyleSheet } from "react-nativescript";
-import { FrameNavigationProp } from "react-nativescript-navigation";
+import * as React from 'react';
+import { StyleSheet } from 'react-nativescript';
+import { FrameNavigationProp } from 'react-nativescript-navigation';
 
-import { MainStackParamList } from "../NavigationParamList";
+import { MainStackParamList } from '../NavigationParamList';
 
 type ScreenOneProps = {
-    route: RouteProp<MainStackParamList, "One">,
-    navigation: FrameNavigationProp<MainStackParamList, "One">,
+  route: RouteProp<MainStackParamList, 'One'>;
+  navigation: FrameNavigationProp<MainStackParamList, 'One'>;
 };
 
 export function ScreenOne({ navigation }: ScreenOneProps) {
-    return (
-        <flexboxLayout style={styles.container}>
-            <label className="text-2xl mb-4 font-bold text-center">
-                Hello World!
-            </label>
-            <button
-                style={styles.button}
-                onTap={() => Dialogs.alert("Tapped!")}
-            >
-                Tap me for an alert
-            </button>
-            <button
-                style={styles.button}
-                onTap={() => navigation.navigate("Two", { message: "Hello, world!" })}
-            >
-                Go to next screen
-            </button>
-        </flexboxLayout>
-    );
+  return (
+    <flexboxLayout style={styles.container}>
+      <label className="text-2xl mb-4 font-bold text-center">
+        Bienvenue sur le TP
+      </label>
+      <label className="text-2xl mb-4 font-bold text-center">
+        React NativeScript
+      </label>
+      <button
+        style={styles.button}
+        onTap={() => navigation.navigate('Two', { ville: 'Grenoble' })}
+      >
+        Météo Grenoble
+      </button>
+    </flexboxLayout>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        height: "100%",
-        flexDirection: "column",
-        justifyContent: "center",
-    },
-    button: {
-        fontSize: 24,
-        color: "#2e6ddf",
-    },
+  container: {
+    height: '100%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  button: {
+    fontSize: 24,
+    color: '#2e6ddf',
+  },
 });
